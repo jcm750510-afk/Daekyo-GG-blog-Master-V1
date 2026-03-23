316a1ae3c310d6c1b3a289ce614642716572f3f3
jcm750510-afk <jcm750510@gmail.com>
2026-03-23 17:05:57 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -22,3 +22,11 @@ export default defineConfig(({mode}) => {
     },
   };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/Daekyo-GG-blog-Master-V1/', // 이 줄을 꼭 추가해 주세요!
+})
