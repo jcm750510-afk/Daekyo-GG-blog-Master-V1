855bb32bc021a0fce0d9d31c4f445d106365295d
jcm750510-afk <jcm750510@gmail.com>
2026-09-17 17:26:44 +0900

    feat: integrate public notice crawler panel

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
+import PublicNoticePanel from './PublicNoticePanel.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <PublicNoticePanel />
     <App />
   </StrictMode>,
 );
